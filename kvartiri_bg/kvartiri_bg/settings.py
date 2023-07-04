a1315a509228b699bbc51ab62daf79e69210b60f
Velin-Todorov <velintodorov@yahoo.com>
2023-07-04 21:18:06 +0200

    changes to vercel again

--- kvartiri_bg/kvartiri_bg/settings.py
+++ kvartiri_bg/kvartiri_bg/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = '*.vercel_app.urls'
+ROOT_URLCONF = 'kvartiri_bg.urls'
 
 TEMPLATES = [
     {
