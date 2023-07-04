632c9d13cf11c9c570e152144fdaa2929a3a230e
Velin-Todorov <velintodorov@yahoo.com>
2023-07-04 21:39:49 +0200

    Vercel changes again

--- kvartiri_bg/kvartiri_bg/urls.py
+++ kvartiri_bg/kvartiri_bg/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from home_view.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home_view.urls'))
+    path('', IndexView.as_view(), name='home')
 ]
