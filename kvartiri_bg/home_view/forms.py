70b94d1a969696fdb17f68a2395401773e9d69ff
Velin-Todorov <velintodorov@yahoo.com>
2023-07-11 23:21:26 +0200

    .

--- kvartiri_bg/home_view/forms.py
+++ kvartiri_bg/home_view/forms.py
@@ -2,10 +2,13 @@ from django import forms
 from .models import Property
 from django.forms import RadioSelect
 
+from consts import CITIES
+
+
 class SearchForm(forms.Form):
     """
     Form that handles searching for a property
     by town
     """
-    town = forms.CharField()
+    town = forms.ChoiceField(choices=CITIES, initial=CITIES[0])
     type = forms.ChoiceField(choices=Property.CHOICES)
