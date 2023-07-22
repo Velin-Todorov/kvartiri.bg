a29e41e040b6d2a004891a7bde0ce7ab46f037f4
Velin Todorov <velintodorov@yahoo.com>
2023-07-22 18:22:24 +0200

    added login and register forms

---
+++ kvartiri_bg/auth_views/forms.py
@@ -0,0 +1,84 @@
+from django.contrib.auth import forms as auth_forms, get_user_model
+from django import forms
+
+UserModel = get_user_model()
+
+class LoginUserForm(auth_forms.AuthenticationForm):
+    email = forms.CharField(
+        label='Email',
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                'placeholder': 'Email Address'
+            }
+        )
+    )
+
+    password = forms.CharField(
+        label='password',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                'placeholder': 'Password'
+            }
+        )
+    )
+        
+
+class RegistrationForm(auth_forms.UserCreationForm):
+    class Meta:
+        model = UserModel
+        fields = '__all__'
+
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                'placeholder': 'First Name'
+            }
+        )
+    )
+
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                'placeholder': 'Last Name'
+            }
+        )
+    )
+
+    email = forms.CharField(
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                'placeholder': 'Email Address'
+            }
+        )
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                'placeholder': 'Password'
+            }
+        )
+    )
+
+    password2= forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                'placeholder': 'Repeat Password'
+            }
+        )
+    )
+
+    def save(self, commit=True):
+        result = super().save(commit)
+        return result
+    
+
+    
