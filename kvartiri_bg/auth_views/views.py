7808006a877f2df8771cb1ad1a7e2a6b63838316
Velin Todorov <velintodorov@yahoo.com>
2023-07-22 23:27:00 +0200

    issues with posting

--- kvartiri_bg/auth_views/views.py
+++ kvartiri_bg/auth_views/views.py
@@ -1,50 +1,41 @@
 from django.forms.models import BaseModelForm
-from django.views import generic as views
+from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.http import HttpResponse
+from .models import User
+
 from .forms import RegistrationForm, LoginUserForm
 from django.contrib.auth import views as auth_views, login, get_user_model
-# Create your views here.
+# Create your views here
 
 
-UserModel = get_user_model()
+# class OnlyAnonymousMixin:
+#     def dispatch(self, request, *args, **kwargs):
+#         if request.user.is_authenticated:
+#             return HttpResponse(self.get_success_url())
 
-class OnlyAnonymousMixin:
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return HttpResponse(self.get_success_url())
-
-        return super().dispatch(request, *args, **kwargs)
+#         return super().dispatch(request, *args, **kwargs)
 
 
-class RegisterUserView(OnlyAnonymousMixin, views.CreateView):
-    template_name = 'registration.html'
+class RegisterUserView(CreateView):
+    template_name_suffix = ''
     form_class = RegistrationForm
+    template_name = 'registration.html'
     success_url = reverse_lazy('login')
-
     
-    def form_valid(self, form: BaseModelForm) -> HttpResponse:
-        result =  super().form_valid(form)
+    def form_valid(self, form):
+        print(form.is_valid())
+        print(form.data)
+        result = super().form_valid(form)
 
         login(self.request, self.object)
 
         return result
 
-    def form_invalid(self, form: BaseModelForm) -> HttpResponse:
+
+    def form_invalid(self, form):
+        print('form is invalid')
         return super().form_invalid(form)
-
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        context['next'] = self.request.GET.get('next', '')
-
-        return context
-    
-
-    def get_success_url(self):
-
-        return self.request.POST.get('next', self.success_url)
 
 
 class LoginUserView(auth_views.LoginView):
