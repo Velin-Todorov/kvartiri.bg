c191f9f9925b2951a8e7c45ca3bfb716c8443147
Velin Todorov <velintodorov@yahoo.com>
2023-07-23 17:56:25 +0200

    Fixed the issue with the registration form

--- kvartiri_bg/auth_views/forms.py
+++ kvartiri_bg/auth_views/forms.py
@@ -28,20 +28,19 @@ class LoginUserForm(auth_forms.AuthenticationForm):
         
 
 class RegistrationForm(auth_forms.UserCreationForm):
-    
     class Meta:
         model = UserModel
         fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
         widgets = {
             'first_name': forms.TextInput(
                 attrs={
-                    'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                    'class': 'relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
                     'placeholder': 'First Name'
                 }
             ),
             'last_name': forms.TextInput(
                 attrs={
-                    'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                    'class': 'relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
                     'placeholder': 'Last Name'
                 }
             ),
@@ -51,71 +50,7 @@ class RegistrationForm(auth_forms.UserCreationForm):
                     'class': 'relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
                     'placeholder': 'Email Address'
                 }
-            ),
-
-            'password1': forms.PasswordInput(
-                attrs={
-                    'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-                    'placeholder': 'Password'
-                }
-            ),
-
-            'password2': forms.PasswordInput(
-                attrs={
-                    'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-                    'placeholder': 'Repeat Password'
-                }
             )
         }   
-
-    def save(self, commit=True):
-        result = super().save(commit)
-        return result
-
-    # first_name = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-    #             'placeholder': 'First Name'
-    #         }
-    #     )
-    # )
-
-    # last_name = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-    #             'placeholder': 'Last Name'
-    #         }
-    #     )
-    # )
-
-    # email = forms.CharField(
-    #     widget=forms.EmailInput(
-    #         attrs={
-    #             'class': 'relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-    #             'placeholder': 'Email Address'
-    #         }
-    #     )
-    # )
-
-    # password1 = forms.CharField(
-    #     widget=forms.PasswordInput(
-    #         attrs={
-    #             'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-    #             'placeholder': 'Password'
-    #         }
-    #     )
-    # )
-
-    # password2= forms.CharField(
-    #     widget=forms.PasswordInput(
-    #         attrs={
-    #             'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-    #             'placeholder': 'Repeat Password'
-    #         }
-    #     )
-    # )
-
 
     
