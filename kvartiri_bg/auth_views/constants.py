7d56817fd8b97e96f8cc99a2729dda687e62b855
Velin Todorov <velintodorov@yahoo.com>
2023-07-23 20:58:46 +0200

    utility file to store constants

---
+++ kvartiri_bg/auth_views/constants.py
@@ -0,0 +1,9 @@
+STUDIO: tuple = 'STUDIO', 'Studio'
+APPARTMENT: tuple = 'APPARTMENT', 'Appartment'
+ROOM: tuple = 'SINGLE_ROOM', 'Single Room'
+
+CHOICES = [
+    STUDIO,
+    APPARTMENT,
+    ROOM
+]
