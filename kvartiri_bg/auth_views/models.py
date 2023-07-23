50ba0743ff05d18288f37a6b1bddb743534b57d3
Velin Todorov <velintodorov@yahoo.com>
2023-07-23 21:01:29 +0200

    model changes

--- kvartiri_bg/auth_views/models.py
+++ kvartiri_bg/auth_views/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .constants import CHOICES
 from home_view.models import Property
 from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager, UserManager
 # Create your models here.
@@ -14,8 +15,6 @@ class User(AbstractUser, PermissionsMixin):
     """
     username= None
     email = models.EmailField(unique=True)
-
-    
     objects = CustomUserManager()
 
     USERNAME_FIELD = "email"
@@ -25,10 +24,13 @@ class UserProfile(models.Model):
     """
     Model that contains app-specific info about the user
     """
+    username  = None
+    looking_for = models.CharField(choices=CHOICES, max_length=11, blank=True, null=True)
+    budget = models.ValueRange(start=0, end=100000)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     is_premium = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
     saved_properties = models.ForeignKey(Property, null=True, blank=True, on_delete=models.DO_NOTHING)
     profile_picture = models.ImageField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = User.email
