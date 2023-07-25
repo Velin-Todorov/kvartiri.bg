f2e869dfc074e0675d5b03c397fe89ea1030e9e2
Velin Todorov <velintodorov@yahoo.com>
2023-07-26 00:20:08 +0200

    changed constants

--- kvartiri_bg/auth_views/constants.py
+++ kvartiri_bg/auth_views/constants.py
@@ -1,9 +1,23 @@
+NO_CHOICES = None, 'Other'
+
 STUDIO: tuple = 'STUDIO', 'Studio'
 APPARTMENT: tuple = 'APPARTMENT', 'Appartment'
 ROOM: tuple = 'SINGLE_ROOM', 'Single Room'
 
 CHOICES = [
+    NO_CHOICES,
     STUDIO,
     APPARTMENT,
     ROOM
+]
+LOW: tuple = '0 Lv. - 500 Lv.', '0 Lv. - 500 Lv.'
+MID: tuple = '500 Lv. - 1000 Lv.', '500 Lv. - 1000 Lv.'
+HIGH: tuple = '1000+ Lv.', '1000+ Lv.'
+
+
+PRICE_RANGE = [
+    NO_CHOICES,
+    LOW,
+    MID, 
+    HIGH
 ]
