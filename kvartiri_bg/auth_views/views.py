62b93566d15e783d066c056fccdb46ab554f8dd5
Velin Todorov <velintodorov@yahoo.com>
2023-07-26 01:21:27 +0200

    Linking user to profile now works.

--- kvartiri_bg/auth_views/views.py
+++ kvartiri_bg/auth_views/views.py
@@ -1,11 +1,10 @@
 from django.forms.models import BaseModelForm
 from django.views.generic.edit import CreateView, FormView
 from django.urls import reverse_lazy
-from django.http import HttpResponse
-from .models import User
 from .forms import RegistrationForm, LoginUserForm, CreateProfileForm
 from django.contrib.auth import views as auth_views, login, get_user_model
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from .models import Profile, User
 # Create your views here
 
 
@@ -32,4 +31,29 @@ class LoginUserView(auth_views.LoginView):
 class CreateProfileView(FormView, PermissionRequiredMixin):
     form_class = CreateProfileForm
     template_name = 'create_profile.html'
-    success_url = reverse_lazy('profile')
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = User.objects.all()
+
+        looking_for = form.cleaned_data['looking_for']
+        about = form.cleaned_data['about']
+        budget = form.cleaned_data['budget']
+        profile_picture = form.cleaned_data['profile_picture']
+
+        profile = Profile.objects.create(
+            looking_for=looking_for,
+            about=about,
+            budget=budget,
+            profile_picture=profile_picture,
+            user_id = user.last().pk
+        )
+        return super().form_valid(form)  
+
+
+    def form_invalid(self, form):
+        print('Invalid')
+        return super().form_invalid(form)
+
+
+
