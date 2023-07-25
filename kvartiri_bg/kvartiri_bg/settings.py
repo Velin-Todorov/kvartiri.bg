34f458829da5c1ccb4f5df2767ffddc67f46b88c
Velin Todorov <velintodorov@yahoo.com>
2023-07-26 00:20:46 +0200

    added urls for media files

--- kvartiri_bg/kvartiri_bg/settings.py
+++ kvartiri_bg/kvartiri_bg/settings.py
@@ -169,6 +169,12 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = '/static'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
+MEDIA_URL = '/media/' # Public URL at the browser
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- kvartiri_bg/kvartiri_bg/urls.py
+++ kvartiri_bg/kvartiri_bg/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home_view.views import IndexView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('auth/', include('auth_views.urls'))
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #new
