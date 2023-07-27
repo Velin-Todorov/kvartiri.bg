ece9becfe81449e3daec0522b59a3ad0248f2324
Velin Todorov <velintodorov@yahoo.com>
2023-07-28 01:23:06 +0200

    added forms and model to handle landlords

--- kvartiri_bg/auth_views/forms.py
+++ kvartiri_bg/auth_views/forms.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import forms as auth_forms, get_user_model
 from django import forms
 from django.forms import ModelForm
-from .models import Profile
-from .constants import CHOICES, PRICE_RANGE
+from .models import Profile, LandlordProfile
+from .constants import CHOICES, PRICE_RANGE, PROFILE_TYPE, TYPE
 
 
 UserModel = get_user_model()
@@ -22,17 +22,18 @@ class LoginUserForm(auth_forms.AuthenticationForm):
         label='password',
         widget=forms.PasswordInput(
             attrs={
-                'class': 'elative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                'class': 'relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
                 'placeholder': 'Password'
             }
         )
     )
+
         
 
 class RegistrationForm(auth_forms.UserCreationForm):
     class Meta:
         model = UserModel
-        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['email', 'password1', 'password2', 'type']
         widgets = {
             'first_name': forms.TextInput(
                 attrs={
@@ -52,16 +53,40 @@ class RegistrationForm(auth_forms.UserCreationForm):
                     'class': 'relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
                     'placeholder': 'Email Address'
                 }
-            )
+            ),
+
+            'type': forms.RadioSelect(choices=PROFILE_TYPE)
         }   
 
 
 class CreateProfileForm(ModelForm):
     class Meta:
         model = Profile
-        fields = ['looking_for', 'about', 'budget', 'profile_picture']
+        fields = [
+            'first_name', 
+            'last_name', 
+            'looking_for', 
+            'about', 
+            'budget', 
+            'profile_picture'
+        ]
+
         widgets = {
             'looking_for': forms.RadioSelect(choices=CHOICES),
             'budget': forms.RadioSelect(choices=PRICE_RANGE)
         }
-    
+
+class CreateLandlordProfileForm(ModelForm):
+    class Meta:
+        model = LandlordProfile
+        fields = [
+            'first_name',
+            'last_name',
+            'about',
+            'profile_picture',
+            'type'
+        ]
+
+        widgets = {
+            'type': forms.RadioSelect(choices=TYPE)
+        }
--- kvartiri_bg/auth_views/models.py
+++ kvartiri_bg/auth_views/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from .constants import CHOICES, PRICE_RANGE
+from .constants import CHOICES, PRICE_RANGE, TYPE, PROFILE_TYPE
 from home_view.models import Property
-from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager, UserManager, User
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager, UserManager
+from datetime import datetime, timezone
 # Create your models here.
 
 
@@ -13,12 +14,13 @@ class User(AbstractUser, PermissionsMixin):
     """
     Custom User model
     """
-    username= None
     email = models.EmailField(unique=True)
+    username = None
+    type = models.CharField(choices=PROFILE_TYPE, max_length=11, default=PROFILE_TYPE[0][0])
     objects = CustomUserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['type']
 
 
 from django.conf import settings
@@ -26,12 +28,38 @@ class Profile(models.Model):
     """
     Model that contains app-specific info about the user
     """
-    looking_for = models.CharField(choices=CHOICES, max_length=11, blank=False, null=True)
-    about = models.TextField(blank=True, null=True)
+    first_name = models.TextField(blank=True, null=True)
+    last_name = models.TextField(blank=True, null=True)
+    looking_for = models.CharField(choices=CHOICES, max_length=11, blank=False)
+    about = models.TextField(blank=True)
+    is_finished = models.BooleanField(default=False)
     budget = models.CharField(choices=PRICE_RANGE, max_length=18, blank=False, null=True)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
     is_verified = models.BooleanField(default=False)
     saved_properties = models.ForeignKey(Property, null=True, blank=True, on_delete=models.DO_NOTHING)
     profile_picture = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+
+    USERNAME_FIELD = 'email'
+
+class LandlordProfile(models.Model):
+    """
+    Model that contains app-specific info about the landlord
+    """
+    first_name = models.TextField(blank=True, null=True)
+    last_name = models.TextField(blank=True, null=True)
+    about = models.TextField(blank=True)
+    is_finished = models.BooleanField(default=False)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+    profile_picture = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+    offerings = models.ForeignKey(Property, null=True, blank=True, on_delete=models.CASCADE)
+    type = models.CharField(choices=TYPE, max_length=15, default=TYPE[0][0])
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def age(self):
+        current_datetime = datetime.now()
+        return (current_datetime - self.created_at).days
 
     USERNAME_FIELD = 'email'
