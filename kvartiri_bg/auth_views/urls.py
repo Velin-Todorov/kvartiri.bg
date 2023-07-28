dd31a5b865bbed8839a9944d0626e6fc5647fa10
Velin Todorov <velintodorov@yahoo.com>
2023-07-28 23:00:27 +0200

    added custom get_success_url()

--- kvartiri_bg/auth_views/urls.py
+++ kvartiri_bg/auth_views/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from .views import LoginUserView, RegisterUserView, CreateProfileView, ChooseProfileView, CreateLandlordProfileView
+from django.urls import path, reverse
+from .views import LoginUserView, RegisterUserView, CreateProfileView, ChooseProfileView, CreateLandlordProfileView, LogoutUserView
 from django.contrib.auth.decorators import login_required, permission_required
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register', RegisterUserView.as_view(), name='register'),
     path('choose_profile', login_required(ChooseProfileView.as_view(), login_url='register'), name='choose_profile'),
     path('finish_landlord_profile', login_required(CreateLandlordProfileView.as_view(), login_url='register'), name='finish_landlord_profile'),
-    path('finish_profile', login_required(CreateProfileView.as_view(), login_url='login'), name='finish_profile')
+    path('finish_profile', login_required(CreateProfileView.as_view(), login_url='login'), name='finish_profile'),
+    path('logout', login_required(LogoutUserView.as_view()), name='logout')
 ]
