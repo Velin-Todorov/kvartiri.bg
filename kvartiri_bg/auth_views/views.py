166d3ea87b2dd6aa3864b71345386f8c62e71863
Velin Todorov <velintodorov@yahoo.com>
2023-07-28 21:17:37 +0200

    Fixed the auth issue. :))

--- kvartiri_bg/auth_views/views.py
+++ kvartiri_bg/auth_views/views.py
@@ -1,16 +1,14 @@
-from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
 from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import redirect
 from .forms import RegistrationForm, LoginUserForm, CreateProfileForm, CreateLandlordProfileForm
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth import views as auth_views, login, get_user_model, authenticate
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from .models import Profile, User, LandlordProfile
 # Create your views here
-
 
 User = get_user_model()
 
@@ -25,28 +23,58 @@ class RegisterUserView(FormView):
     def form_valid(self, form):
         email = form.cleaned_data['email']
         password = form.cleaned_data['password1']
+        type = form.cleaned_data['type']
 
         user = User(
             email=email, 
-            password=make_password(password)
+            password=make_password(password),
+            type=type 
         )
         user.save()
 
         login(self.request, user)
+        if type == 'LANDLORD':
+            return redirect('finish_landlord_profile')
+
+        elif type == 'TENANT':
+            return redirect('finish_profile') 
+
 
     def form_invalid(self, form):
         return super().form_invalid(form)
 
 
-class LoginUserView(auth_views.LoginView):
+class LoginUserView(FormView):
     template_name = 'login.html'
-    authentication_form = LoginUserForm
-    redirect_authenticated_user = False
+    form_class = LoginUserForm
+    success_url = reverse_lazy('home')
 
-    def form_invalid(self, form: AuthenticationForm) -> HttpResponse:
-        print(form.errors)
+
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+
+        user = authenticate(self.request, email=email, password =password)
+
+        if user is not None:
+            login(self.request, user)
+
+            if user.profile_finished:
+                return redirect(reverse('profile/<int:pk>', args=user.pk))
+            else:
+                if user.type == 'TENANT':
+                    return redirect('finish_profile')
+                elif user.type == 'LANDLORD':
+                    return redirect('finish_landlord_profile')
+        
+        return super().form_valid(form)
+
+
+
+    def form_invalid(self, form) -> HttpResponse:
         return super().form_invalid(form)
 
+        
 class LogoutUserView(auth_views.LogoutView):
     pass
 
@@ -57,10 +85,10 @@ class CreateProfileView(FormView, PermissionRequiredMixin):
     """
     form_class = CreateProfileForm
     template_name = 'create_profile.html'
-    success_url = reverse_lazy('profile')
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
-        user = User.objects.all()
+        user = User.objects.filter(type='TENANT')
 
         first_name = form.cleaned_data['first_name']
         last_name = form.cleaned_data['last_name']
@@ -79,6 +107,7 @@ class CreateProfileView(FormView, PermissionRequiredMixin):
             profile_picture=profile_picture,
             user_id = user.last().pk
         )
+        user.last().profile_finished = True
         return super().form_valid(form)  
 
 
@@ -94,10 +123,10 @@ class ChooseProfileView(TemplateView):
 class CreateLandlordProfileView(FormView, PermissionRequiredMixin):
     form_class = CreateLandlordProfileForm
     template_name = 'create_landlord_profile.html'
-    success_url = reverse_lazy('landlord_profile')
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
-        user = User.objects.all()
+        user = User.objects.filter(type='LANDLORD')
 
         first_name = form.cleaned_data['first_name']
         last_name = form.cleaned_data['last_name']
@@ -114,7 +143,7 @@ class CreateLandlordProfileView(FormView, PermissionRequiredMixin):
             type = type,
             user_id = user.last().pk
         )
-
+        user.last().profile_finished = True
         return super().form_valid(form)  
 
 
