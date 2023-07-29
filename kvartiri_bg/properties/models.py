60859a60b127da2368f8ff88bc86685cd046d97f
Velin Todorov <velintodorov@yahoo.com>
2023-07-29 16:01:05 +0200

    added Propreties app

---
+++ kvartiri_bg/properties/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from auth_views.constants import CHOICES
+from auth_views.models import LandlordProfile
+
+# Create your models here.
+class Property(models.Model):
+    """
+    Model that represents a Property of different type
+    """
+    type = models.CharField(choices=CHOICES, default='ROOM')
+    price_per_month = models.DecimalField(max_digits=20, decimal_places=2)
+    landlord = models.ForeignKey(LandlordProfile, on_delete=models.CASCADE)
+    size = models.IntegerField()
+    town = models.CharField() #dropdown field
+    address = models.TextField()
+    furnished = models.BooleanField()
+    utilities_included = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
---
+++ kvartiri_bg/properties/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+class CreateProperty:
+    """
+    Handles new property creation
+    """
+    pass
+
+class EditProperty:
+    """
+    Handles existing property editing
+    """
+    pass
+
+class DeleteProperty:
+    """
+    Handles property deletion
+    """
+    pass
+
+
