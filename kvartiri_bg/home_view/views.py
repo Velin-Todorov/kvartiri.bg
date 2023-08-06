f0e7c52c5c8c3275e1923bb45d2e1cb59086c8fc
Velin Todorov <velintodorov@yahoo.com>
2023-08-06 11:13:22 +0200

    minor change

--- kvartiri_bg/home_view/views.py
+++ kvartiri_bg/home_view/views.py
@@ -1,9 +1,16 @@
+from typing import Any, Dict
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
+
 
 class ChooseYourProfile(TemplateView):
     template_name = 'pre_register.html'
