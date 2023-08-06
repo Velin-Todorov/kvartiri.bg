3fd240639fc0db99da43f0aa3f4a1560382966f9
Velin Todorov <velintodorov@yahoo.com>
2023-08-07 01:02:53 +0200

    created admin page

--- kvartiri_bg/auth_views/admin.py
+++ kvartiri_bg/auth_views/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import LandlordProfile, Profile, User
+
 
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(LandlordProfile)
+class LandlordProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
