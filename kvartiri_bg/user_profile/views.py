2a487572e924e2a2099d517c498a2f76b3fa7dd3
Velin Todorov <velintodorov@yahoo.com>
2023-08-06 11:14:00 +0200

    added class factory

--- kvartiri_bg/user_profile/views.py
+++ kvartiri_bg/user_profile/views.py
@@ -1,13 +1,45 @@
+from typing import Any, Dict, Optional, Type
+from django.forms.models import BaseModelForm
 from django.shortcuts import render
-from django.views.generic import DetailView, UpdateView, TemplateView
-from auth_views.models import Profile, LandlordProfile
+from django.views.generic import DetailView, UpdateView, TemplateView, DeleteView
+from django.contrib.auth.views import PasswordChangeView
+from auth_views.models import *
+from django import forms
+from .forms import *
+from django.urls import reverse, reverse_lazy
+from auth_views.forms import CreateLandlordProfileForm, CreateProfileForm
+from .mixins import UserContextMixin, SuccessMixin
 
+
+def create_profile_update_view(model_class, form_klass, template):
+    class UpdateViewClass(UserContextMixin, SuccessMixin, UpdateView):
+        model = model_class
+        form_class = form_klass
+        template_name = template
+    
+    return UpdateViewClass
+
+def create_delete_profile_view(model_class):
+    class DeleteProfile(DeleteView):
+        model = model_class
+        success_url = reverse_lazy('login')
+        template_name = 'confirm_delete.html'
+    
+    return DeleteProfile
 
 # Create your views here.
-class ProfileView(DetailView):
+class ProfileView(UserContextMixin, DetailView):
     model = Profile
     context_object_name = 'profile'
     template_name = 'profile_page.html'
+
+
+
+class LandlordProfileView(DetailView):
+    model = LandlordProfile
+    context_object_name = 'landlord'
+    template_name = 'landlord_profile.html'
+    succes_url = reverse_lazy('home')
 
 
 class MessagesView(DetailView):
@@ -16,26 +48,37 @@ class MessagesView(DetailView):
     """
     pass
 
+
+class LandlordMessages(DetailView):
+    """
+    Landlord Messages
+    """
+    pass
+
+
+
 class FavouritesView(DetailView):
     """
     View that displays user's favourite properties
     """
     pass
 
-class EditProfile(UpdateView):
+class LandlordOfferings(DetailView):
     """
-    View that handles profile updates
+    Landlord's offerings
     """
     pass
 
+class ChangePasswordView(SuccessMixin, PasswordChangeView):
+    """
+    Handles password change.
+    """
+    template_name = 'change_password.html'
 
-class PasswordResestView:
-    """
-    Handles password reset
-    """
 
-class OffersView:
-    """
-    View that displays landlord's offerings
-    """
+EditTenantProfile = create_profile_update_view(Profile, CreateProfileForm, 'edit_profile.html')
+EditLandlordProfile = create_profile_update_view(LandlordProfile, CreateLandlordProfileForm, 'edit_profile.html')
+DeleteTenantProfile = create_delete_profile_view(Profile)
+DeleteLandlordProfile = create_delete_profile_view(LandlordProfile)
+
 
