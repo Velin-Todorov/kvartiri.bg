d6482493ffe57cf7151e1a8e62066e15695d3b87
Velin Todorov <velintodorov@yahoo.com>
2023-08-07 01:07:10 +0200

    created a message model

--- kvartiri_bg/user_profile/models.py
+++ kvartiri_bg/user_profile/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from auth_views.models import Profile, LandlordProfile
+
+
+class Messages(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    recipient = models.ForeignKey(LandlordProfile, on_delete=models.CASCADE)
+    msg_content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
