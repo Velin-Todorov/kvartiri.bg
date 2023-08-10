0e1b8e1ea64a51626d5ad289977ed6d5539d4883
Velin Todorov <velintodorov@yahoo.com>
2023-08-10 19:46:10 +0200

    Used mixins to refactor

--- kvartiri_bg/home_view/views.py
+++ kvartiri_bg/home_view/views.py
@@ -3,46 +3,21 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView
 from auth_views.models import LandlordProfile, Profile
 from properties.models import Property
+from mixins import GetContextBasedOnType
 
 # Create your views here.
-class IndexView(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context['user'] = self.request.user
-
-        if self.request.user.is_authenticated:
-            if self.request.user.profile_finished:
-                if self.request.user.type == 'LANDLORD':
-                    context['landlord_profile'] = LandlordProfile.objects.get(user_id=self.request.user.pk)
-                else:
-                    context['profile'] = Profile.objects.get(user_id=self.request.user.pk)
-
-        return context
-
+class IndexView(GetContextBasedOnType, TemplateView):
+    template_name = 'home_templates/home.html'
 
 class ChooseYourProfile(TemplateView):
     template_name = 'pre_register.html'
         
     
-class DisplayProperties(ListView):
+class DisplayProperties(GetContextBasedOnType, ListView):
     model = Property
     paginate_by = 10
-    template_name = 'all_properties.html'
+    template_name = 'property_templates/all_properties.html'
     context_object_name = 'properties'
-
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        
-        if self.request.user.is_authenticated:
-            if self.request.user.type == 'LANDLORD':
-                context['landlord_profile'] = LandlordProfile.objects.get(user_id=self.request.user.pk)
-            else:
-                context['profile'] = Profile.objects.get(user_id=self.request.user.pk)
-
-        return context
-
 
 class RentOutPlace(TemplateView):
     template_name = 'rent_out.html'
