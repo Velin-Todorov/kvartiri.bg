734897855bfb978d264bba7015f74ceff03b0da3
Velin Todorov <velintodorov@yahoo.com>
2023-08-10 16:32:39 +0200

    removed messages model

--- kvartiri_bg/user_profile/models.py
+++ kvartiri_bg/user_profile/models.py
@@ -2,8 +2,3 @@ from django.db import models
 from auth_views.models import Profile, LandlordProfile
 
 
-class Messages(models.Model):
-    sender = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    recipient = models.ForeignKey(LandlordProfile, on_delete=models.CASCADE)
-    msg_content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
