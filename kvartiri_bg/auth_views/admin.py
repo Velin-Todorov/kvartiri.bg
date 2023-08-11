fbfac225802c7a1a9816bc81efb0aecfe1f7dc98
Velin Todorov <velintodorov@yahoo.com>
2023-08-11 21:00:00 +0200

    changes to the admin panel

--- kvartiri_bg/auth_views/admin.py
+++ kvartiri_bg/auth_views/admin.py
@@ -5,15 +5,15 @@ from .models import LandlordProfile, Profile, User
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name')
-    search_fields = ('first_name')
-    list_filter = ('location')
+    list_display = ['first_name', 'last_name']
+    search_fields = ['first_name']
+    list_filter = ['location']
 
 @admin.register(LandlordProfile)
 class LandlordProfileAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name')
-    search_fields = ('first_name')
-    list_filter = ('location')
+    list_display = ['first_name', 'last_name']
+    search_fields = ['first_name']
+    list_filter = ['location']
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
--- kvartiri_bg/properties/admin.py
+++ kvartiri_bg/properties/admin.py
@@ -4,10 +4,10 @@ from .models import Property, MessageFromLandlord, MessageFromTenant
 # Register your models here.
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('type', 'landlord', 'size', 'town')
-    list_filter = ('town')
-    ordering = ('created_at')
-    sortable_by = ('price_per_month')
+    list_display = ['type', 'landlord', 'size', 'town']
+    list_filter = ['town']
+    ordering = ['created_at']
+    sortable_by = ['price_per_month']
 
 
     
